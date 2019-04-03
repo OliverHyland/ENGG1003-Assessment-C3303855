a64eb94011f8e11158e9327370538f28523553b2
Oliver Hyland <oliverwhyland@gmail.com>
2019-04-03 08:50:05 +0000

    End of first assessment lab. Summarised and understood assessment brief.  Worked through understanding expression containing modulus, conducted pseudo-code considering how to convert ASCII characters to values, how to apply a change to those values, and how to display that. Came to ideas about using strings (which are arrays which make values of text), using a pointer to go to each  element in the array and add the key to it, and incrementing. Noted that %s can be used to print and scan strings.

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
-main ()
+/*the first couple of hours of lab 1 were spent understanding and simplifying the
+dense brief for the assessment task. Beyond that, research was taken into the types of ciphers
+and pseudocode was begun. This is why this initial commit does not contain much code*/
+
+/* resolved confusion regarding e(x) = (m + k) mod 26, recognizing it does not multiply by the modulus but
+this is a simplified way of stating that the value needs to be tested and adjusted if it falls outside the range
+of 0 and 25. Additionally studied lecture content and wrote pseudocode for an array, using a pointer to an element,
+adding the key to it, and the incrementing the pointer to the next element, exiting at the end of the array */
+
+//Task 1: Encryption of a message with a rotation cipher given the message text and rotation amount
+
+int main ()
 {
+  printf("Hello world!\n");  
+  printf("Hello world!\n"); 
+  printf("Hello world!\n"); 
+  printf("Hello world!\n"); 
   return 0;
-  //new line 1 test
-  //git commit test 2, che to repository 
+  
+  char string[] = {         }; 
+  string + i = string + i + k;
+  i++;
 }

829634f5c47f6e8df3bc7d2ced8de5059678e65b
Oliver Hyland <oliverwhyland@gmail.com>
2019-04-11 23:59:25 +0000

    Re-load completely attempt 1

--- main.c
+++ main.c
@@ -2,23 +2,19 @@
 
 int main ()
 {
+    FILE*input;
+    input = fopen("input", "r");
+    char key = 3;
     
-    
-FILE*filepointer;                                      //file pointer to the data type FILE
-filepointer = fopen("input", "r");         // the file named input of data type FILE, open it for reading
-Int key = (a number)                             //declare your key for the encryption
-Int counter = 0;                                      //declare and initialise the counter which will be used to point in the array
-While (feof(input)==0)                         //so long it is not true that we are at the end of the file, execute the loop:
-Char asciiFriend;                                   //declaring a variable of data type char
-Fscanf(input, "%c", &asciiFriend);      //read a character from file input of type char and send it to the variable asciiFriend
-asciiFriend = asciiFriend + key;         //an ASCII character has been given a value after being read as char, now add the key
-Counter++;                                           //increase your counter by the value 
-
-
-
-Char str[1024];  //scanning the first element in the array str
-Scanf("%s", str);  //%s prints and scans strings as ASCII even though they are values right now
-
-
-
+    int counter = 0;
+    int text[1000];
+    while (feof(input)==0)
+    {
+        fscanf(input, "%d", &text);
+        char i = text + counter*2;
+        char text[i] = text + key;
+        counter++;
+    }
+    for (counter = 0; counter < 1000; counter = (counter + 2))
+        printf("%c", text[counter]);
 }
